f9644950c417f3f2bbef75f84a8260171dab8ca4
Jordan Perrone <64396016+jperrone27@users.noreply.github.com>
2025-04-26 20:52:41 -0400

    bot runner added

---
+++ bot-runner.py
@@ -0,0 +1,27 @@
+
+from dotenv import dotenv_values
+from owlmind.pipeline import ModelProvider
+from owlmind.simple import SimpleEngine
+from owlmind.discord import DiscordBot
+
+if __name__ == '__main__':
+
+    # load token from .env
+    config = dotenv_values('.env')
+    TOKEN = config['DISCORD_TOKEN']
+    URL = config['SERVER_URL']
+    MODEL = config['SERVER_MODEL'] if 'SERVER_MODEL' in config else None
+    TYPE = config['SERVER_TYPE'] if 'SERVER_TYPE' in config else None
+    API_KEY = config['SERVER_API_KEY'] if 'SERVER_API_KEY' in config else None
+
+    # Configure a ModelProvider if there is an URL
+    provider = ModelProvider(type=TYPE,  base_url=URL, api_key=API_KEY, model=MODEL) if URL else None
+
+    # Load Simples Bot Brain loading rules from a CSV
+    engine = SimpleEngine(id='bot-1')
+    engine.model_provider = provider
+    engine.load('rules/bot-rules-3.csv')
+
+    # Kick start the Bot Runner process
+    bot = DiscordBot(token=TOKEN, engine=engine, debug=True)
+    bot.run()
